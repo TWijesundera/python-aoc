84730f5bcffcb5ef4b6d9367657b7e8b95bb4e69
TWijesundera <thisarawijesundera@gmail.com>
2023-12-04 20:56:13 -0500

    gear ratios wip

---
+++ 3 - Gear Ratios/test.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class TestGearRatios(unittest.TestCase):
+    def test_parse_numbers_in_line(self):
+        input = "467..114.."
+
+
+if __name__ == "__main__":
+    unittest.main()
